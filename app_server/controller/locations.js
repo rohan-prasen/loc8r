b63a004e335d2c3376250ea08292cd63824d3249
rohan-prasen <rohanprasen19@gmail.com>
2023-08-10 10:36:58 +0530

    Changing location.js and others.js

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -8,6 +8,14 @@ module.exports.locationInfo = function(req, res){
     res.render('location-info', { title: 'Location info'});
 };
 
+module.exports.locationInfo2 = function(req, res){
+    res.render('location-info2', { title: 'Location info'});
+};
+
+module.exports.locationInfo3 = function(req, res){
+    res.render('location-info3', { title: 'Location info'});
+};
+
 /* GET 'ADD review' page */
 module.exports.addReview = function(req, res){
     res.render('location-review-form', { title: 'Add Review'});
